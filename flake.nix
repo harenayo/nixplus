db0536c2e4e3ecb3332c0cf343512c825e17a96f
Harenayo <123649644+harenayo@users.noreply.github.com>
2024-05-08 23:21:51 +0000

    Add 'myshell', a Home Manager module

--- flake.nix
+++ flake.nix
@@ -2,13 +2,39 @@
   outputs = {
     ...
   }: {
-    homeModules.cohm = {
-      lib,
-      ...
-    }: {
-      options.core = lib.options.mkOption {
-        default = {};
+    homeModules = {
+      cohm = {
+        lib,
+        ...
+      }: {
+        options.nixplus.cohm = lib.options.mkOption {
+          default = {};
+        };
       };
+      myshell = {
+        config,
+        lib,
+        ...
+      }: {
+        options.nixplus.myshell = {
+          enable = lib.options.mkOption {
+            default = false;
+            type = lib.types.bool;
+          };
+          package = lib.options.mkOption {
+            type = lib.types.package;
+          };
+        };
+        config.programs.bash = lib.modules.mkIf config.nixplus.myshell.enable {
+          enable = true;
+          initExtra = let sh = "${config.nixplus.myshell.package}${config.nixplus.myshell.package.shellPath}"; in lib.modules.mkOrder 10000 "SHELL=${sh} exec ${sh}";
+        };
+      };
+    };
+    lib.homeConfiguration = {
+      modules,
+    }: {
+      imports = modules;
     };
     nixosModules.cohm = {
       config,
@@ -19,7 +45,7 @@
         default = false;
         type = lib.types.bool;
       };
-      config.users.users = lib.modules.mkIf config.nixplus.cohm.enable (builtins.mapAttrs (_: user: user.core) (lib.attrsets.filterAttrs (_: user: user ? core) config.home-manager.users));
+      config.users.users = lib.modules.mkIf config.nixplus.cohm.enable (builtins.mapAttrs (_: user: user.nixplus.cohm) (lib.attrsets.filterAttrs (_: user: user ? nixplus.cohm) config.home-manager.users));
     };
   };
 }
