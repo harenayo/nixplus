968e985f4995acacadf3e67f7b1107c93c281308
Harenayo <123649644+harenayo@users.noreply.github.com>
2024-05-10 15:16:18 +0000

    Fix an infinite recursion

--- flake.nix
+++ flake.nix
@@ -2,33 +2,23 @@
   outputs = {
     ...
   }: {
-    homeModules = {
-      cohm = {
-        lib,
-        ...
-      }: {
-        options.nixplus.cohm = lib.options.mkOption {
-          default = {};
+    homeModules.myshell = {
+      config,
+      lib,
+      ...
+    }: {
+      options.nixplus.myshell = {
+        enable = lib.options.mkOption {
+          default = false;
+          type = lib.types.bool;
+        };
+        package = lib.options.mkOption {
+          type = lib.types.package;
         };
       };
-      myshell = {
-        config,
-        lib,
-        ...
-      }: {
-        options.nixplus.myshell = {
-          enable = lib.options.mkOption {
-            default = false;
-            type = lib.types.bool;
-          };
-          package = lib.options.mkOption {
-            type = lib.types.package;
-          };
-        };
-        config.programs.bash = lib.modules.mkIf config.nixplus.myshell.enable {
-          enable = true;
-          initExtra = let sh = "${config.nixplus.myshell.package}${config.nixplus.myshell.package.shellPath}"; in lib.modules.mkOrder 10000 "SHELL=${sh} exec ${sh}";
-        };
+      config.programs.bash = lib.modules.mkIf config.nixplus.myshell.enable {
+        enable = true;
+        initExtra = let sh = "${config.nixplus.myshell.package}${config.nixplus.myshell.package.shellPath}"; in lib.modules.mkOrder 10000 "SHELL=${sh} exec ${sh}";
       };
     };
     lib.homeConfiguration = {
@@ -40,13 +30,26 @@
       cohm = {
         config,
         lib,
+        options,
         ...
       }: {
         options.nixplus.cohm.enable = lib.options.mkOption {
           default = false;
           type = lib.types.bool;
         };
-        config.users.users = lib.modules.mkIf config.nixplus.cohm.enable (builtins.mapAttrs (_: user: user.nixplus.cohm) (lib.attrsets.filterAttrs (_: user: user ? nixplus.cohm) config.home-manager.users));
+        config = lib.modules.mkIf config.nixplus.cohm.enable {
+          home-manager.sharedModules = [
+            ({
+              lib,
+              ...
+            }: {
+              options.nixplus.cohm = lib.options.mkOption {
+                default = builtins.head options.user.user.getSubModules;
+              };
+            })
+          ];
+          users.users = builtins.mapAttrs (_: user: user.nixplus.cohm) (lib.attrsets.filterAttrs (_: user: user ? nixplus.cohm) config.home-manager.users);
+        };
       };
       ssv = {
         config,
@@ -57,9 +60,11 @@
           default = false;
           type = lib.types.bool;
         };
-        config.home-manager.users = lib.modules.mkIf config.nixplus.ssv.enable (builtins.mapAttrs (_: _: {
-          home.stateVersion = config.system.stateVersion;
-        }) config.home-manager.users);
+        config.home-manager.sharedModules = lib.modules.mkIf config.nixplus.ssv.enable [
+          {
+            home.stateVersion = config.system.stateVersion;
+          }
+        ];
       };
     };
   };
