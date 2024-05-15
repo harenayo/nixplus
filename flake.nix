956c1521f88ce65f88f2a2e85e471846d755c63b
Harenayo <123649644+harenayo@users.noreply.github.com>
2024-05-15 04:51:30 +0000

    Add a formatter

--- flake.nix
+++ flake.nix
@@ -1,70 +1,52 @@
 {
-  outputs = {
-    ...
-  }: {
-    homeModules.myshell = {
-      config,
-      lib,
-      ...
-    }: {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs = { nixpkgs, ... }: {
+    formatter = builtins.mapAttrs (_: pkgs: pkgs.nixfmt) nixpkgs.legacyPackages;
+    homeModules.myshell = { config, lib, ... }: {
       options.nixplus.myshell = {
         enable = lib.options.mkOption {
           default = false;
           type = lib.types.bool;
         };
-        package = lib.options.mkOption {
-          type = lib.types.package;
-        };
+        package = lib.options.mkOption { type = lib.types.package; };
       };
       config.programs.bash = lib.modules.mkIf config.nixplus.myshell.enable {
         enable = true;
-        initExtra = let sh = "${config.nixplus.myshell.package}${config.nixplus.myshell.package.shellPath}"; in lib.modules.mkOrder 10000 "SHELL=${sh} exec ${sh}";
+        initExtra = let
+          sh =
+            "${config.nixplus.myshell.package}${config.nixplus.myshell.package.shellPath}";
+        in lib.modules.mkOrder 10000 "SHELL=${sh} exec ${sh}";
       };
     };
-    lib.homeConfiguration = {
-      modules,
-    }: {
-      imports = modules;
-    };
+    lib.homeConfiguration = { modules, }: { imports = modules; };
     nixosModules = {
-      cohm = {
-        config,
-        lib,
-        options,
-        ...
-      }: {
+      cohm = { config, lib, options, ... }: {
         options.nixplus.cohm.enable = lib.options.mkOption {
           default = false;
           type = lib.types.bool;
         };
         config = lib.modules.mkIf config.nixplus.cohm.enable {
           home-manager.sharedModules = [
-            ({
-              lib,
-              ...
-            }: {
+            ({ lib, ... }: {
               options.nixplus.cohm = lib.options.mkOption {
                 default = builtins.head options.user.user.getSubModules;
               };
             })
           ];
-          users.users = builtins.mapAttrs (_: user: user.nixplus.cohm) (lib.attrsets.filterAttrs (_: user: user ? nixplus.cohm) config.home-manager.users);
+          users.users = builtins.mapAttrs (_: user: user.nixplus.cohm)
+            (lib.attrsets.filterAttrs (_: user: user ? nixplus.cohm)
+              config.home-manager.users);
         };
       };
-      ssv = {
-        config,
-        lib,
-        ...
-      }: {
+      ssv = { config, lib, ... }: {
         options.nixplus.ssv.enable = lib.options.mkOption {
           default = false;
           type = lib.types.bool;
         };
-        config.home-manager.sharedModules = lib.modules.mkIf config.nixplus.ssv.enable [
-          {
+        config.home-manager.sharedModules =
+          lib.modules.mkIf config.nixplus.ssv.enable [{
             home.stateVersion = config.system.stateVersion;
-          }
-        ];
+          }];
       };
     };
   };
